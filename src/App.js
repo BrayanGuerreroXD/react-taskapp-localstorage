13d93016e98c18a40fa370912cdb59506aef8e09
BrayanGuerreroXD <bg7047653@gmail.com>
2023-02-08 16:26:08 -0500

    Add form into TaskCreator component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
-import { useState } from "react";
 import "./App.css";
+import { TaskCreator } from "./components/TaskCreator";
 
 function App() {
-  const [newTaskName, setNewTaskName] = useState();
-
-  const handleSubmit = (e) => {
-    e.preventDefault()
-    localStorage.setItem('tasks', newTaskName)
-    setNewTaskName('')
-  }
-
   return (
     <div className="App">
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          placeholder="Enter a new task"
-          onChange={(e) => setNewTaskName(e.target.value)}
-        />
-        <button>Save task</button>
-      </form>
+      <TaskCreator />
     </div>
   );
 }
